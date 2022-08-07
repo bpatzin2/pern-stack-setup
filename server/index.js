9a25e3fbaac6a30b9d65a6064673eca51e442955
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2022-08-07 10:25:24 +0200

    CREATE operation works

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors())
@@ -11,7 +12,12 @@ app.use(express.json())
 // create a todo
 app.post("/todos", async (req, res) => {
   try{
-    console.log(req.body)
+    const {description} = req.body
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    )
+    res.json(newTodo.rows[0])
   } catch(err){
     console.log(err.message)
   }
